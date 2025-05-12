397028952e986afd33dbe40d5b88ea1cb6685804
MedAliHaBloomwell <mohamed.hamza@bloomwell.eu>
2025-05-12 13:25:37 +0200

    feat: add payment result handling and UI feedback in drop-in widget

--- example/lib/src/drop_in.dart
+++ example/lib/src/drop_in.dart
@@ -10,6 +10,7 @@ class DropInWidget extends StatefulWidget {
 }
 
 class _DropInWidgetState extends State<DropInWidget> {
+  final amount = 10000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +21,9 @@ class _DropInWidgetState extends State<DropInWidget> {
               client: AdyenClient(
                 baseUrl: 'http://192.168.178.26:3000',
               ),
-              amount: 10000,
+              amount: amount,
               reference: '2222',
+              acceptOnlyCard: true,
               configuration: AdyenConfiguration(
                 clientKey: "test_4ZDD22772FAUDI4BURXBGDXOCY5AO53R",
                 adyenAPI: "http://192.168.178.26:3000",
@@ -29,10 +31,105 @@ class _DropInWidgetState extends State<DropInWidget> {
                 redirectURL:
                     '${kIsWeb || kIsWasm ? 'https://app.staging.bloomwell.de/checkout?shopperOrder=2222' : 'adyenExample://com.example.adyenExample'}/checkout?shopperOrder=2222',
               ),
-              onPaymentResult: (payment) {},
+              onPaymentResult: (payment) {
+                switch (payment) {
+                  case PaymentAdvancedFinished():
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: const Icon(
+                            Icons.check_circle,
+                            color: Colors.green,
+                            size: 64,
+                          ),
+                          title: const Text('Payment successful'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    break;
+                  case PaymentSessionFinished():
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: const Icon(
+                            Icons.check_circle,
+                            color: Colors.green,
+                            size: 64,
+                          ),
+                          title: const Text('Payment successful'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    break;
+                  case PaymentCancelledByUser():
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: const Icon(
+                            Icons.error,
+                            color: Colors.orangeAccent,
+                            size: 56,
+                          ),
+                          title: const Text('Payment cancelled'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    break;
+                  case PaymentError():
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                            size: 56,
+                          ),
+                          title: const Text('Payment Failed'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    break;
+                }
+              },
             );
           },
-          child: const Text('Drop-in widget'),
+          child: Text('Pay now ${amount / 100}€'),
         ),
       ),
     );
