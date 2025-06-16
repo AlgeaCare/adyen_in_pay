3eef360692b92c4a67bc9a8cc3b230382ae38d6d
MedAliHaBloomwell <mohamed.hamza@bloomwell.eu>
2025-06-16 19:00:26 +0200

    create adyen key configuration class

---
+++ adyen_client_api/lib/src/models/adyen_keys_configuration.dart
@@ -0,0 +1,36 @@
+class AdyenKeysConfiguration {
+  final String clientKey;
+  final String appleMerchantId;
+  final String merchantName;
+
+  const AdyenKeysConfiguration({
+    required this.clientKey,
+    required this.appleMerchantId,
+    required this.merchantName,
+  });
+
+  factory AdyenKeysConfiguration.fromJson(Map<String, dynamic> json) => AdyenKeysConfiguration(
+        clientKey: json['ADYEN_CLIENT_KEY'],
+        appleMerchantId: json['APPLE_MERCHANT_ID'],
+        merchantName: json['MERCHANT_NAME'],
+      );
+
+  @override
+  String toString() {
+    return 'AdyenConfiguration(clientKey: $clientKey, appleMerchantId: $appleMerchantId, merchantName: $merchantName)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is AdyenKeysConfiguration &&
+        clientKey == other.clientKey &&
+        appleMerchantId == other.appleMerchantId &&
+        merchantName == other.merchantName;
+  }
+
+  @override
+  int get hashCode {
+    return clientKey.hashCode ^ appleMerchantId.hashCode ^ merchantName.hashCode;
+  }
+}
