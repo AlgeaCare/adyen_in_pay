c450cfa13b67072e938ed65a91010fa185d9032f
artem kidanov <artem.kydanov@gmail.com>
2025-06-24 12:44:29 +0200

    Added export for payments_page_response model in payment_client_api.dart

--- adyen_client_api/lib/payment_client_api.dart
+++ adyen_client_api/lib/payment_client_api.dart
@@ -7,4 +7,5 @@ export 'src/models/payment_response.dart';
 export 'src/models/session_response.dart';
 export 'src/models/shopper_billing_address.dart';
 export 'src/models/payment_information.dart';
+export 'src/models/payments_page_response.dart';
 export 'src/models/common.dart';
