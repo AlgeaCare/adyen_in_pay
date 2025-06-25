c8f9240f0e7be6e10a7c1ce88498b011ba2b43fb
artem kidanov <artem.kydanov@gmail.com>
2025-06-25 14:43:35 +0200

    Refactor debug logging in dropInAdvancedMobile to improve clarity by directly logging payment method values instead of their types

--- lib/src/platform/drop_in/mobile_drop_in.dart
+++ lib/src/platform/drop_in/mobile_drop_in.dart
@@ -107,7 +107,7 @@ Future<void> dropInAdvancedMobile({
           ..putIfAbsent('returnUrl', () => configuration.redirectURL);
 
         final meths = paymentMethods?.onlyCards().values.map((e) => e['type']);
-        debugPrint('__methods ${meths}');
+        debugPrint('__methods ${paymentMethods?.onlyCards().values}');
 
         if (paymentMethods
                 ?.onlyCards()
