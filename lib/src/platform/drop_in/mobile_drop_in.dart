d84df40ca2d3b346f97e661d5714f3308e13b070
MedAliHaBloomwell <Mohamed.Hamza@bloomwell.eu>
2025-08-29 17:33:29 +0200

    fix: uncomment and update GooglePay configuration with dynamic merchant values

--- lib/src/platform/drop_in/mobile_drop_in.dart
+++ lib/src/platform/drop_in/mobile_drop_in.dart
@@ -120,19 +120,20 @@ Future<void> dropInAdvancedMobile({
       allowOnboarding: true,
     ),
     googlePayConfiguration: GooglePayConfiguration(
-      //merchantAccount: configuration.adyenKeysConfiguration.googleMerchantId,
+      merchantAccount:
+          paymentMethods.googlePayConfiguration?["getwayMerchantId"] ??
+          configuration.adyenKeysConfiguration.googleMerchantId,
       googlePayEnvironment:
           configuration.env == 'test' ? GooglePayEnvironment.test : GooglePayEnvironment.production,
       allowedCardNetworks: paymentMethods.googlePayBrandsConfiguration,
-      // merchantInfo: MerchantInfo(
-      //   merchantName:
-      //       paymentMethods.googlePayConfiguration?["getwayMerchantId"] ??
-      //       configuration.adyenKeysConfiguration.merchantName,
-      //   merchantId:
-      //       configuration
-      //           .adyenKeysConfiguration
-      //           .googleMerchantId, // paymentMethods.googlePayConfiguration?["merchantId"],,
-      // ),
+      merchantInfo: MerchantInfo(
+        merchantName:
+            paymentMethods.googlePayConfiguration?["getwayMerchantId"] ??
+            configuration.adyenKeysConfiguration.merchantName,
+        merchantId:
+            paymentMethods.googlePayConfiguration?["merchantId"] ??
+            configuration.adyenKeysConfiguration.googleMerchantId, // ,
+      ),
     ),
     storedPaymentMethodConfiguration: StoredPaymentMethodConfiguration(
       showPreselectedStoredPaymentMethod: true,
