f302d15757bb173c60cd7d5d804d6416eca591c4
MedAliHaBloomwell <Mohamed.Hamza@bloomwell.eu>
2025-08-29 16:37:50 +0200

    fix: enable merchant info configuration for Google Pay integration

--- lib/src/platform/drop_in/mobile_drop_in.dart
+++ lib/src/platform/drop_in/mobile_drop_in.dart
@@ -120,18 +120,21 @@ Future<void> dropInAdvancedMobile({
       allowOnboarding: true,
     ),
     googlePayConfiguration: GooglePayConfiguration(
-      merchantAccount: configuration.adyenKeysConfiguration.googleMerchantId,
+      merchantAccount:
+          paymentMethods.googlePayConfiguration?["merchantId"] ??
+          configuration.adyenKeysConfiguration.googleMerchantId,
       googlePayEnvironment:
           configuration.env == 'test' ? GooglePayEnvironment.test : GooglePayEnvironment.production,
       allowedCardNetworks: paymentMethods.googlePayBrandsConfiguration,
-      // merchantInfo: MerchantInfo(
-      //   merchantName:
-      //       paymentMethods.googlePayConfiguration?["getwayMerchantId"] ??
-      //       configuration.adyenKeysConfiguration.merchantName,
-      //   merchantId:
-      //       paymentMethods
-      //           .googlePayConfiguration?["merchantId"], //configuration.adyenKeysConfiguration.googleMerchantId, // ,
-      // ),
+      merchantInfo: MerchantInfo(
+        merchantName:
+            paymentMethods.googlePayConfiguration?["getwayMerchantId"] ??
+            configuration.adyenKeysConfiguration.merchantName,
+        merchantId:
+            configuration
+                .adyenKeysConfiguration
+                .googleMerchantId, // paymentMethods.googlePayConfiguration?["merchantId"],,
+      ),
     ),
     storedPaymentMethodConfiguration: StoredPaymentMethodConfiguration(
       showPreselectedStoredPaymentMethod: true,
